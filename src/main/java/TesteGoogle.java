7629c8bd9b64e5c03e362c872ba8990c4920c247
igorsilva37 <igordsa18@gmail.com>
2021-02-09 11:25:31 -0300

    Alteração do Método e implementação do Junit

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,9 +1,11 @@
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 	
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 //		System.setProperty("webdriver.gecko.driver","C:\\Users\\Igor\\Desktop\\geckodriver-v0.29.0-win64.zip");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
